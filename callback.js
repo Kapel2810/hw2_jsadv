05ba78e3afc5a99c92c077d39c5c8908c14ea3ea
Katrina Apel <kapel2810@gmail.com>
2021-11-21 08:41:19 +0200

    ex 2 done

--- callback.js
+++ callback.js
@@ -1,18 +1,41 @@
 
 let mainParagraphElement = document.getElementById('main-p')
 let today = new Date()
+let TIME_IN_SECONSDS = 3
 
 // ------- ex1---------
-//get current time  with callback
+//get current time  (sync) 
+// const runCode = () => {
+//     showTime(getTime)
+
+// }
+
+// const getTime = () => {
+//    let time = today.getHours() + ':' +today.getMinutes() + ':' + today.getSeconds()
+//    mainParagraphElement.innerHTML = time
+//    console.log(time)
+// }
+
+// const showTime = (callback, time) => {
+//     console.log('start')
+//     callback(time)
+//     console.log('end')
+// }
+
+//----------ex3
+//get curent time acync with timeout 3 sec
+
 const runCode = () => {
     showTime(getTime)
 
 }
 
 const getTime = () => {
-   let time = today.getHours() + ':' +today.getMinutes() + ':' + today.getSeconds()
-   mainParagraphElement.innerHTML = time
-   console.log(time)
+    setTimeout(() => {
+        let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+        mainParagraphElement.innerHTML = time
+        console.log(time)
+    }, TIME_IN_SECONSDS * 1000);
 }
 
 const showTime = (callback, time) => {
