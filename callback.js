368547c63dc6d5a3c6471037650f6082af8ce749
Katrina Apel <kapel2810@gmail.com>
2021-11-21 13:30:30 +0200

    ex3 done

--- callback.js
+++ callback.js
@@ -1,9 +1,11 @@
 
 let mainParagraphElement = document.getElementById('main-p')
 let mainDivElement = document.getElementById('main-div')
+let numberInputElement = document.getElementById('number-input')
 let today = new Date()
-let TIME_IN_SECONSDS = 3
+let TIME_IN_SECONDS = 3
 let TIME_PER_SECOND = 1000
+
 
 // ------- ex1---------
 //get current time  (sync) 
@@ -37,7 +39,7 @@ let TIME_PER_SECOND = 1000
 //         let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
 //         mainParagraphElement.innerHTML = time
 //         console.log(time)
-//     }, TIME_IN_SECONSDS * 1000);
+//     }, TIME_IN_SECONDS * 1000);
 // }
 
 // const showTime = (callback, time) => {
@@ -48,34 +50,53 @@ let TIME_PER_SECOND = 1000
 
 //----------------ex 3---------------
 
+const runCode = () => {
+    showTime(getTime)
+}
+const getTime = () => {
+    let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+    setTimeout(() => {
+        mainParagraphElement.innerHTML = time
+        console.log(time)
+    }, TIME_IN_SECONDS * 1000);
+}
+
+const showTime = (callback, time) => {
+ console.log('start');
+ callback(time)
+ console.log('end');
+}
+
 
 //--------------ex4-----------------
 // set time out 3 sec 
 
-const runCode = () => {
-    showRandomNumber(getRandomNumber)
-}
+// const runCode = () => {
+//     showRandomNumber(getRandomNumber)
+// }
 
-const getRandomNumber = () => {
-    setTimeout(() => {
-        let randomNumber = Math.floor(Math.random() * 100)
-        mainDivElement.innerHTML = randomNumber
-        console.log(randomNumber)
-    }, TIME_PER_SECOND * 3);
-    setTimeout(() => {
-        let randomNumber = Math.floor(Math.random() * 100)
-        mainDivElement.innerHTML = randomNumber
-        console.log(randomNumber)
-    }, TIME_PER_SECOND *5);
-    setTimeout(() => {
-        let randomNumber = Math.floor(Math.random() * 100)
-        mainDivElement.innerHTML = randomNumber
-        console.log(randomNumber)
-    }, TIME_PER_SECOND *7);
-}
+// const getRandomNumber = () => {
+//     setTimeout(() => {
+//         let randomNumber = Math.floor(Math.random() * 100)
+//         mainDivElement.innerHTML = randomNumber
+//         console.log(randomNumber)
+//     }, TIME_PER_SECOND * 3);
+//     setTimeout(() => {
+//         let randomNumber = Math.floor(Math.random() * 100)
+//         mainDivElement.innerHTML = randomNumber
+//         console.log(randomNumber)
+//     }, TIME_PER_SECOND *5);
+//     setTimeout(() => {
+//         let randomNumber = Math.floor(Math.random() * 100)
+//         mainDivElement.innerHTML = randomNumber
+//         console.log(randomNumber)
+//     }, TIME_PER_SECOND *7);
+// }
 
-const showRandomNumber = (callback) => {
-    console.log("start")
-    callback()
-    console.log('end')
-}
+// const showRandomNumber = (callback) => {
+//     console.log("start")
+//     callback()
+//     console.log('end')
+// }
+
+//-------- ex5--------
